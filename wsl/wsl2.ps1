1bf2c99dc494fe27065ed07800ee4094834e6ce2
GitKitNet <101803117+GitKitNet@users.noreply.github.com>
2022-05-22 07:38:55 +0300

    Update and rename run.ps1 to wsl2.ps1

--- wsl/wsl2.ps1
+++ wsl/wsl2.ps1
@@ -1,10 +1,12 @@
-winget install wingetcreate -h  --accept-package-agreements --accept-source-agreements
-
+<#
+ * Install WSL2 on windows
+#>
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 wsl --set-default-version 2
 wsl --list --online
 wsl --update
+
 $DistroName="Ubuntu-20.04"
 wsl --install -d ${DistroName}
 wsl -s ${DistroName}
